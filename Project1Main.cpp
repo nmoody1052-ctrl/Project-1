4987d2c693743b5105673d9255e367a571ef5795
nmoody1052-ctrl <nmoody1052@gmail.com>
2026-09-16 12:52:17 -0500

    Update Project1Main.cpp

--- Project1Main.cpp
+++ Project1Main.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include "resource.h"
+#include "resources.h"
+#include "accountsystem.h"
+using namespace std;
 
+//Prints Header
+void header(){
+        cout<<
+        "+-----------------------------------------------+\n"<<
+        "|        Computer Science and Engieering        |\n"<<
+        "|        CSCE 2110 - Computer Science __        |\n"<<
+        "|  Nathan Moody NJM 0184 NathanMoody@my.unt.edu |\n"<<
+        "+-----------------------------------------------+\n\n";
+}
+//End
+
+//Prints Menu
+void menu(){
+        cout<<
+        "*********************** Menu **********************\n"<<
+        "| 1.                                              |\n"<<
+        "***************************************************\n"<<
+        ">> ";
+
+}
+//End
+
+int main(){
