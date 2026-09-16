83827db892fc19ef68544cfb264582f254bfd2c9
nmoody1052-ctrl <nmoody1052@gmail.com>
2026-09-16 12:56:51 -0500

    Update Reservation.cpp

--- Reservation.cpp
+++ Reservation.cpp
@@ -1,4 +1,4 @@
-#include "Reservation.h"
+#include "reservation.h"
 
 Reservation::Reservation(int resId, int sId, int rId, std::string time)
     : reservationId(resId), studentId(sId), resourceId(rId), timestamp(time) {}
