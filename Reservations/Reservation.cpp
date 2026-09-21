30288f6fc0df0502232c0ec44b1e4701c5619b8f
Mahamat <241851095+mmm0766@users.noreply.github.com>
2026-09-20 21:18:55 -0500

    Refactor Reservation constructor parameters

--- Reservations/Reservation.cpp
+++ Reservations/Reservation.cpp
@@ -1,9 +1,9 @@
 #include "reservation.h"
 
-Reservation::Reservation(int resId, int sId, std::string rId, std::string name, std::string time)
-    : reservationId(resId), studentId(sId), resourceId(rId), studentName(name), timestamp(time) {}
+Reservation::Reservation(int resId, int sId, std::string sName,
+                          std::string rId, std::string time)
+    : reservationId(resId), studentId(sId), studentName(sName),
+      resourceId(rId), timestamp(time) {}
 
 ReservationNode::ReservationNode(const Reservation& res)
-    : data(res), next(nullptr) {} 
-
-
+    : data(res), next(nullptr) {}
